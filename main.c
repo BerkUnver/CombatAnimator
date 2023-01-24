f144789d80966e18b976e55e6fe6ea07a12bf955
Yeasteroni <31731914+BerkUnver@users.noreply.github.com>
2023-01-24 13:33:07 -0500

    Added header row for textboxes

--- main.c
+++ main.c
@@ -21,19 +21,27 @@
 #define SELECT_BUTTON MOUSE_BUTTON_LEFT
 #define UNDO_KEY KEY_Z
 #define UNDO_KEY_MODIFIER KEY_LEFT_CONTROL
-#define REDO_KEY_MODIFIER KEY_LEFT_ALT
+#define REDO_KEY_MODIFIER KEY_LEFT_SHIFT
 #define SCALE_SPEED 0.75f
 #define FRAME_WIDTH 160
 #define TEXTURE_HEIGHT_IN_WINDOW 0.5f
 #define FRAME_TIME 0.1
 
 #define BACKGROUND_COLOR GRAY
+#define FONT_COLOR RAYWHITE
+
 const Hitbox DEFAULT_HITBOX = {40, 40, 24};
 #define FRAME_ROW_COLOR (Color) {50, 50, 50, 255}
 #define FRAME_ROW_SIZE 32
 #define FRAME_RHOMBUS_RADIUS 12
 #define FRAME_RHOMBUS_UNSELECTED_COLOR HITBOX_CIRCLE_INACTIVE_COLOR
 #define FRAME_RHOMBUS_SELECTED_COLOR RAYWHITE
+
+#define HITBOX_HEADER_MARGIN_X 8
+#define HITBOX_HEADER_MARGIN_Y 4
+#define HITBOX_HEADER_TEXT "Hitboxes"
+
+#define HITBOX_ROW_SIZE 32
 
 void DrawRhombus(Vector2 pos, float xSize, float ySize, Color color) {
     Vector2 topPoint = {pos.x, pos.y - ySize};
@@ -49,23 +57,34 @@ int main() {
     SetTargetFPS(60);
 
     Texture2D sprite = LoadTexture(TEST_IMAGE_PATH);
-
+    const int fontSize = GetFontDefault().baseSize;
+    
+    const int hitboxHeaderHeight = fontSize + HITBOX_HEADER_MARGIN_Y * 2; 
+    const int timelineHeight = FRAME_ROW_SIZE + hitboxHeaderHeight + HITBOX_ROW_SIZE;
+    const int timelineY = WINDOW_Y - timelineHeight;
+    const int hitboxHeaderY = timelineY + FRAME_ROW_SIZE;
+    const int hitboxHeaderTextX = HITBOX_HEADER_MARGIN_X;
+    // const int hitboxHeaderTextWidth = MeasureText(HITBOX_HEADER_TEXT, fontSize);
+    // const int hitboxHeaderTextHeight = hitboxHeaderHeight;
+    const int hitboxHeaderTextY = hitboxHeaderY + HITBOX_HEADER_MARGIN_Y;
+    const int hitboxRowY = hitboxHeaderY + hitboxHeaderHeight;
+    
     float spriteScale = WINDOW_Y * TEXTURE_HEIGHT_IN_WINDOW / sprite.height;
     Vector2 spritePos = {(WINDOW_X - FRAME_WIDTH * spriteScale) / 2.0f, (WINDOW_Y - sprite.height * spriteScale) / 2.0f};
 
     
     int frameIdx = 0;
     int frameCount = sprite.width / FRAME_WIDTH;
-    EditorState editorState = {DEFAULT_HITBOX, calloc(frameCount, sizeof(bool)), frameCount};
-    EditorHistory history = AllocEditorHistory(&editorState);
+    EditorState state = {DEFAULT_HITBOX, calloc(frameCount, sizeof(bool)), frameCount};
+    EditorHistory history = AllocEditorHistory(&state);
 
-    typedef enum State {
+    typedef enum Mode {
         IDLE = 0,
         PLAYING = 1,
         DRAGGING_HANDLE = 2,
         PANNING_SPRITE = 3
-    } State;
-    State state = IDLE;
+    } Mode;
+    Mode mode = IDLE;
     float playingFrameTime = 0.0f;
     Handle draggingHandle = NONE;
     Vector2 panningSpriteLocalPos = VECTOR2_ZERO;
@@ -76,16 +95,16 @@ int main() {
 
         Vector2 mousePos = GetMousePosition();
         
-        if (state == DRAGGING_HANDLE) {
+        if (mode == DRAGGING_HANDLE) {
             if (IsMouseButtonReleased(SELECT_BUTTON)) {
-                CommitState(&history, &editorState);
-                state = IDLE;
+                CommitState(&history, &state);
+                mode = IDLE;
             } else {
-                SetHitboxHandle(mousePos, spritePos, spriteScale, &editorState.hitbox, draggingHandle); // not handling illegal handle set for now b/c it shouldn't happen.
+                SetHitboxHandle(mousePos, spritePos, spriteScale, &state.hitbox, draggingHandle); // not handling illegal handle set for now b/c it shouldn't happen.
             }
-        } else if (state == PANNING_SPRITE) {
+        } else if (mode == PANNING_SPRITE) {
             if (IsMouseButtonReleased(SELECT_BUTTON)) {
-                state = IDLE;
+                mode = IDLE;
             } else {
                 float globalPanX = spritePos.x + panningSpriteLocalPos.x * spriteScale;
                 float globalPanY = spritePos.y + panningSpriteLocalPos.y * spriteScale;
@@ -93,40 +112,40 @@ int main() {
                 spritePos.y += mousePos.y - globalPanY;
             }
         } else if (IsKeyPressed(UNDO_KEY) && IsKeyDown(UNDO_KEY_MODIFIER)) { // undo
-            state = IDLE;
+            mode = IDLE;
             ChangeOptions option = UNDO;
             if (IsKeyDown(REDO_KEY_MODIFIER)) option = REDO;
 
-            ChangeState(&history, &editorState, option);
+            ChangeState(&history, &state, option);
 
-            int maxIdx = editorState.frameCount - 1;
+            int maxIdx = state.frameCount - 1;
             if (frameIdx > maxIdx) frameIdx = maxIdx;
 
         } else if (IsMouseButtonPressed(SELECT_BUTTON)) {
-            if (WINDOW_Y - FRAME_ROW_SIZE - HITBOX_ROW_SIZE < mousePos.y && mousePos.y <= WINDOW_Y) {
-                if (0.0f <= mousePos.x && mousePos.x < FRAME_ROW_SIZE * editorState.frameCount) { // toggle whether frame is active
-                    state = IDLE;
-                    frameIdx = Clamp((int) mousePos.x / FRAME_ROW_SIZE, 0, editorState.frameCount - 1); // clamp just to be safe
-                    if (mousePos.y > WINDOW_Y - HITBOX_ROW_SIZE) {
-                        editorState.hitboxActiveFrames[frameIdx] = !editorState.hitboxActiveFrames[frameIdx];
-                        CommitState(&history, &editorState);
+            if (timelineY < mousePos.y && mousePos.y <= WINDOW_Y) {
+                if (0.0f <= mousePos.x && mousePos.x < FRAME_ROW_SIZE * state.frameCount) { // toggle whether frame is active
+                    mode = IDLE;
+                    frameIdx = Clamp((int) mousePos.x / FRAME_ROW_SIZE, 0, state.frameCount - 1); // clamp just to be safe
+                    if (mousePos.y > hitboxRowY) {
+                        state.hitboxActiveFrames[frameIdx] = !state.hitboxActiveFrames[frameIdx];
+                        CommitState(&history, &state);
                     };
                 }
             } else {
-                draggingHandle = SelectHitboxHandle(mousePos, spritePos, spriteScale, editorState.hitbox);
+                draggingHandle = SelectHitboxHandle(mousePos, spritePos, spriteScale, state.hitbox);
                 if (draggingHandle != NONE) {
-                    state = DRAGGING_HANDLE;
+                    mode = DRAGGING_HANDLE;
                 } else {
-                    state = PANNING_SPRITE;
+                    mode = PANNING_SPRITE;
                     panningSpriteLocalPos.x = (mousePos.x - spritePos.x) / spriteScale;
                     panningSpriteLocalPos.y = (mousePos.y - spritePos.y) / spriteScale;
                 }
             }
         } else if (IsKeyPressed(PLAY_ANIMATION_KEY)) {
-            if (state == PLAYING) {
-                state = IDLE;
+            if (mode == PLAYING) {
+                mode = IDLE;
             } else {
-                state = PLAYING;
+                mode = PLAYING;
                 playingFrameTime = 0.0f;
             }
         } else if (GetMouseWheelMove() != 0.0f) {
@@ -139,22 +158,22 @@ int main() {
         } else {
             int direction = (IsKeyPressed(NEXT_FRAME_KEY) ? 1 : 0) - (IsKeyPressed(PREVIOUS_FRAME_KEY) ? 1 : 0);
             if (direction) {
-                state = IDLE;
+                mode = IDLE;
                 int newFrame = frameIdx + direction;
 
-                if (newFrame < 0) frameIdx = editorState.frameCount - 1;
-                else if (newFrame >= editorState.frameCount) frameIdx = 0;
+                if (newFrame < 0) frameIdx = state.frameCount - 1;
+                else if (newFrame >= state.frameCount) frameIdx = 0;
                 else frameIdx = newFrame;
             }
         }
 
-        if (state == PLAYING) {
+        if (mode == PLAYING) {
             playingFrameTime += GetFrameTime();
             if (playingFrameTime >= FRAME_TIME)
             {
                 playingFrameTime = fmod(playingFrameTime, FRAME_TIME);
                 frameIdx++;
-                if (frameIdx >= editorState.frameCount) frameIdx = 0;
+                if (frameIdx >= state.frameCount) frameIdx = 0;
             }
         }
         
@@ -165,20 +184,23 @@ int main() {
         Rectangle dest = {spritePos.x, spritePos.y, FRAME_WIDTH * spriteScale, sprite.height * spriteScale};
         DrawTexturePro(sprite, source, dest, VECTOR2_ZERO, 0.0f, WHITE);
         
-        if (editorState.hitboxActiveFrames[frameIdx]) {
-            DrawHitbox(spritePos, spriteScale, editorState.hitbox);
+        if (state.hitboxActiveFrames[frameIdx]) {
+            DrawHitbox(spritePos, spriteScale, state.hitbox);
         }
 
-        DrawRectangle(0, WINDOW_Y - FRAME_ROW_SIZE - HITBOX_ROW_SIZE, WINDOW_X, FRAME_ROW_SIZE + HITBOX_ROW_SIZE, FRAME_ROW_COLOR);
-        for (int i = 0; i < editorState.frameCount; i++) {
+
+        DrawRectangle(0, timelineY, WINDOW_X, timelineHeight, FRAME_ROW_COLOR); // draw timeline background
+        DrawText(HITBOX_HEADER_TEXT, hitboxHeaderTextX, hitboxHeaderTextY, fontSize, FONT_COLOR);
+        
+        for (int i = 0; i < state.frameCount; i++) {
             int xPos = i * FRAME_ROW_SIZE + FRAME_ROW_SIZE / 2;
     
-            Color hitboxColor = editorState.hitboxActiveFrames[i] ? HITBOX_CIRCLE_ACTIVE_COLOR : HITBOX_CIRCLE_INACTIVE_COLOR;
-            int hitboxYPos = WINDOW_Y - HITBOX_ROW_SIZE / 2;
+            Color hitboxColor = state.hitboxActiveFrames[i] ? HITBOX_CIRCLE_ACTIVE_COLOR : HITBOX_CIRCLE_INACTIVE_COLOR;
+            int hitboxYPos = hitboxRowY + HITBOX_ROW_SIZE / 2;
             DrawCircle(xPos, hitboxYPos, HITBOX_CIRCLE_RADIUS, hitboxColor);
 
             Color frameColor = i == frameIdx ? FRAME_RHOMBUS_SELECTED_COLOR : FRAME_RHOMBUS_UNSELECTED_COLOR;
-            Vector2 frameCenter = {xPos, WINDOW_Y - HITBOX_ROW_SIZE - FRAME_ROW_SIZE / 2};
+            Vector2 frameCenter = {xPos, timelineY + FRAME_ROW_SIZE / 2};
             DrawRhombus(frameCenter, FRAME_RHOMBUS_RADIUS, FRAME_RHOMBUS_RADIUS, frameColor);
         }
 
@@ -187,7 +209,7 @@ int main() {
     
     UnloadTexture(sprite);
     FreeEditorHistory(history);
-    free(editorState.hitboxActiveFrames);
+    free(state.hitboxActiveFrames);
     CloseWindow();
     return EXIT_SUCCESS;
 }
